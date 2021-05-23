9cee232d90cc5f1b53d67aac91f318b13e01612f
hy <zihong2020@foxmail.com>
2021-05-24 00:26:19 +0800

    feat(all): 细化对用户的处理，增加去重

--- src/index.js
+++ src/index.js
@@ -4,20 +4,30 @@ const xlsx = require('node-xlsx');
 const fs = require('fs');
 
 // npx playwright codegen --target javascript -o 'temp.js' -b chromium https://m.weibo.cn/
-const userList_07  = []
-const userList_06  = []
-const searchText = '创造营成团名单'
-async function content(){
-    const browser = await chromium.launch({headless:false,slowMo:1000});
+// const userList_07  = []
+// const userList_06  = []
+const userList_bluev = []// 蓝V
+const userList_goldv_static  = []// 金V
+const userList_yellowv  = []// 黄V
+// const userArr = ['蓝V','金V','黄V']
+// const searchText = '小龙虾一斤降价十几元'
+/**
+ * @description: playwright and cheerio
+ * @param {string} url 话题所在地址
+ * @param {number} pagination 统计页数
+ * @return {arr} userList
+ */
+async function content(url,pagination){
+    const browser = await chromium.launch({slowMo:1000});
     
     const page = await browser.newPage();
   
-    const url = `https://m.weibo.cn/`
+    // const url = `https://m.weibo.cn/search?containerid=100103type%3D1%26t%3D10%26q%3D%23%E4%B8%8A%E7%99%BE%E5%90%8D%E5%85%B1%E5%92%8C%E5%85%9A%E4%BA%BA%E8%A6%81%E6%B1%82%E8%AF%A5%E5%85%9A%E4%B8%8E%E7%89%B9%E6%9C%97%E6%99%AE%E5%86%B3%E8%A3%82%23&isnewpage=1&extparam=seat%3D1%26source%3Dranklist%26filter_type%3Drealtimehot%26pos%3D0%26pre_seqid%3D1940032114%26dgr%3D0%26c_type%3D30%26mi_cid%3D100103%26flag%3D1%26cate%3D0%26display_time%3D1620994529&luicode=10000011&lfid=231583`
     await page.goto(url);
 
-    await page.waitForTimeout(3000)
+    // await page.waitForTimeout(3000)
 
-    page.click(`text=#${searchText}#`)
+    // page.click(`text=#${searchText}#`)
 
     // 等待三秒
     await page.waitForTimeout(3000)
@@ -27,7 +37,7 @@ async function content(){
 
 
     // 把鼠标移到页面视图最下方
-    for(let i =0,len = 30;i<=len;i++){
+    for(let i =0,len = pagination;i<=len;i++){
       await page.evaluate(()=> window.scrollTo(0,document.body.scrollHeight))
       await page.waitForTimeout(2000)
     }
@@ -37,17 +47,36 @@ async function content(){
     // console.log(content);
 
     const $ = cheerio.load(content)
-    $('.m-text-cut').each(async(i,elem)=>{
+    $('.m-avatar-box').each(async(i,elem)=>{
       const userBox = $(elem).html()
-      if(userBox.indexOf('<i class="m-icon m-icon-vipl7"></i>')!==-1){
-        const user = $(elem).text()
-        // console.log('vip 7',user);
-        userList_07.push(user)
+      // console.log(userBox);
+      // if(userBox.indexOf('<i class="m-icon m-icon-vipl7"></i>')!==-1){
+      //   const user = $(elem).text()
+      //   console.log('vip 7 ===> ',user);
+      //   userList_07.push(user)
+      // }
+      // if(userBox.indexOf('<i class="m-icon m-icon-vipl6"></i>')!==-1){
+      //   const user = $(elem).text()
+      //   console.log('vip 6 ===> ',user);
+      //   userList_06.push(user)
+      // }
+      // 蓝V
+      if(userBox.indexOf('<i class="m-icon m-icon-bluev"></i>')!==-1){
+        const user = await setUser($(elem).text()) 
+        // console.log('vip bluev ===> ',user);
+        userList_bluev.push(user)
       }
-      if(userBox.indexOf('<i class="m-icon m-icon-vipl6"></i>')!==-1){
-        const user = $(elem).text()
-        // console.log('vip 6',user);
-        userList_06.push(user)
+      // 金V
+      if(userBox.indexOf('<i class="m-icon m-icon-goldv-static"></i>')!==-1){
+        const user = await setUser($(elem).text()) 
+        // console.log('vip goldv-static ===> ',user);
+        userList_goldv_static.push(user)
+      }
+      // 黄V
+      if(userBox.indexOf('<i class="m-icon m-icon-yellowv"></i>')!==-1){
+        const user = await setUser($(elem).text()) 
+        // console.log('vip goldv-static ===> ',user);
+        userList_yellowv.push(user)
       }
       
     })
@@ -55,68 +84,73 @@ async function content(){
     await browser.close();
 }
 
-async function write_xlsx(){
-  //写入Excel数据
-  //excel数据
-  var excelData = [];
-  {
-  //表1
-      //添加数据
-      var addInfo = {};
-      //名称
-      addInfo.name = "VIP_06";
-      //数据数组
-      addInfo.data = [
-          ["x", "用户昵称"],
-      ];
+async function setUser(user){
+  return await {user:user.split('          ')[1],desc:user}
+}
 
-      //添加数据
-      for(let i =0,len=userList_06.length;len>i;i++){
-        addInfo.data.push([i,userList_06[i]]);
-      }
-      addInfo.data.push(['总人数为:',userList_06.length]);
 
-      //添加数据
-      excelData.push(addInfo);
-    }
-    {
-      //表2
-          //添加数据
-          var addInfo = {};
-          //名称
-          addInfo.name = "VIP_07";
-          //数据数组
-          addInfo.data = [
-              ["x", "用户昵称"],
-          ];
-    
-          //添加数据
-          for(let i =0,len=userList_07.length;len>i;i++){
-            addInfo.data.push([i,userList_07[i]]);
+function unique(arr){            
+  for(var i=0; i<arr.length; i++){
+      for(var j=i+1; j<arr.length; j++){
+          if(arr[i].user==arr[j].user){ 
+              arr.splice(j,1);
+              j--;
           }
-          addInfo.data.push(['总人数为:',userList_07.length]);
-    
-          //添加数据
-          excelData.push(addInfo);
-        }
+      }
+  }
+return [...arr];
+}
+
+// 初始化xlsx数据
+async function init_xlsx_data() {
+  const userList =  [
+    {name:'bluev',title:'蓝V',arr:unique(userList_bluev)},
+    {name:'goldv_static',title:'金V',arr:unique(userList_goldv_static)},
+    {name:'yellowv',title:'黄V',arr:unique(userList_yellowv)},
+  ]
+  // console.log(userList);
+  let excelData = []
+  for(let i = 0,len=userList.length;i<len;i++){
+    const temp = userList[i]
+    // console.log(temp);
+    let addInfo = {};
+    addInfo.name = temp.title
+    addInfo.data = [
+      [temp.title, "用户昵称",'备注'],
+    ]
+    for(let y = 0,len=temp.arr.length;y<len;y++){
+      // console.log([y,temp.arr[y]]);
+      addInfo.data.push([y,temp.arr[y].user,temp.arr[y].desc]);
+      // console.log(111,addInfo);
+    }
+    addInfo.data.push(['总计：',`${temp.arr.length}个`,''])
+    excelData.push(addInfo);
+  }
+  // console.log(111,excelData);
+  await write_xlsx(excelData)
+}
+
+async function write_xlsx(excelData){
     // 写xlsx
     var buffer = xlsx.build(excelData);
     //写入数据
-    const fileName =  `./话题_${searchText}.xls`
+    const fileName =  `./话题统计.xls`
     fs.writeFile(fileName, buffer, function (err) {
         if (err)
         {
             throw err;
         }
         //输出日志
-        console.log(`话题_${searchText}已搞定`);
+        console.log(`话题已搞定`);
     })
 }
 
 async function main(){
+  const url = `https://m.weibo.cn/search?containerid=100103type%3D1%26t%3D10%26q%3D%23%E4%B8%8A%E7%99%BE%E5%90%8D%E5%85%B1%E5%92%8C%E5%85%9A%E4%BA%BA%E8%A6%81%E6%B1%82%E8%AF%A5%E5%85%9A%E4%B8%8E%E7%89%B9%E6%9C%97%E6%99%AE%E5%86%B3%E8%A3%82%23&isnewpage=1&extparam=seat%3D1%26source%3Dranklist%26filter_type%3Drealtimehot%26pos%3D0%26pre_seqid%3D1940032114%26dgr%3D0%26c_type%3D30%26mi_cid%3D100103%26flag%3D1%26cate%3D0%26display_time%3D1620994529&luicode=10000011&lfid=231583`
+  const pagination = 10
   console.time('func');
-  await content()
-  await write_xlsx()
+  await content(url,pagination)
+  await init_xlsx_data()
   console.timeEnd('func');
 }
 main()
